feab59db57e5666856b49eb872d7b06b37be6ac1
Venkatarajesh Jakka <venkatarajeshjakka@venkatajeshsair.home>
2019-05-04 18:24:14 -0400

    updated header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
+import logo from './logo.svg';
 import Footer from './components/Footer.jsx';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>React App</p>
+        <img src={logo} className="App-logo" alt="logo" />
+          <h2>React Portfolio</h2>
       </header>
 
       <Footer/>
